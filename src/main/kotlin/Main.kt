48f635a5cafcd707e421243c52bd4316a48f2ba0
Manuel <127980333+ManuelAmayaOrozco@users.noreply.github.com>
2024-03-14 17:47:44 +0100

    Ejercicio 2 Finalizado
    Ejercicio 2 completado con un sistema de IDs automáticos para los libros, además de la opción de añadir un libro desde cero por el menú de usuario sin tener que hacerlo anteriormente.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
-package org.pebiblioteca
-
 fun main() {
-    println("Hello World!")
+
+    //AHORA PODEMOS AÑADIR LIBROS TANTO POR AQUÍ COMO POR EL MENÚ DE USUARIO.
+    //Los IDs se generan de manera automatica con cada libro que sea agregado.
+
+    val gestorBiblioteca = GestorBiblioteca() //Creación del gestor de biblioteca
+
+    val libro = Libro("Prueba", "Autor Prueba", 2005, "Tema Prueba")
+
+    gestorBiblioteca.agregarLibro(libro)
+
+    // AHORA YA SOLO NECESITAMOS EL MENÚ DE USUARIO
+    MenuBiblioteca().accederBiblioteca(gestorBiblioteca)
 }
